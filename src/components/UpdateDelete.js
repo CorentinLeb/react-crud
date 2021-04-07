5ba3261c6bd704c8f650a9d6bf6dff873ff84011
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-04-07 16:13:46 +0200

    working on crud

--- src/components/UpdateDelete.js
+++ src/components/UpdateDelete.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import firebase from "../utils/firebaseConfig";
 
-const UpdateDelete = () => {
-    return (
-        <div>
-            
+const UpdateDelete = ({item}) => {
+  const [update, setUpdate] = useState(false);
+  const [authorUpdate, setAuthorUpdate] = useState(null);
+  const [textUpdate, setTextUpdate] = useState(null);
+
+  const updateItem = () => {
+    let quote = firebase.database().ref("quotesDB").child(item.id);
+    if (authorUpdate !== null) {
+      quote.update({
+        author: authorUpdate,
+      });
+    }
+    if (textUpdate !== null) {
+      quote.update({
+        text: textUpdate,
+      });
+    }
+    setUpdate(false);
+  };
+  return (
+    <li>
+      {update === false && (
+        <div className="item-container">
+          <p>"{item.text}"</p>
+          <h6>{item.author}</h6>
         </div>
-    );
+      )}
+    </li>
+  );
 };
 
 export default UpdateDelete;
